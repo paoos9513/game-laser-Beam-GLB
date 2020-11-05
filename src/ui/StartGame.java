1787f8a153dfbe44df7807aad0269af38b5cfd50
MitoCode <61301128+paoos9513@users.noreply.github.com>
2020-11-04 20:12:52 -0500

    'Ya sirven las funciones de disparar y adivinar un espejo'

--- src/ui/StartGame.java
+++ src/ui/StartGame.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class StartGame {
     Scanner scan = new Scanner(System.in);
     LinkedListMatrix lm;
+    String nickname;
 
     public StartGame() {
         System.out.println("Enter the nickname, row, column and the number of mirrors followed by spaces");
@@ -14,12 +15,13 @@ public class StartGame {
 
         String[] data = scan.nextLine().split(" ");
         if (data.length == 4) {
-            String nickname = data[0];
+            nickname = data[0];
             int row = Integer.parseInt(data[1]);
             int column = Integer.parseInt(data[2]);
             int mirror = Integer.parseInt(data[3]);
             if ((row > 0) && (column > 0 && column <= 26) && (mirror > 0 && mirror <= (row * column))) {
                 lm = new LinkedListMatrix(row, column, mirror);
+                System.out.println(nickname);
                 System.out.println(lm.toString());
 
             } else {
@@ -37,23 +39,45 @@ public class StartGame {
     public void shootLaserBeam() {
         System.out.println("(1..n).Enter the row number\n" + "(A-Z).The column letter\n");
         String action = scan.nextLine();
-
         if (action.length() > 0) {
-            lm.shootLaserAction(action);
-            //lm.selectNode(action);
-            System.out.println(lm.toString());
-
             /**
-             int rowSelect = Integer.parseInt(action.substring(0,action.length()-1));
-             //System.out.println(rowSelect + " " + action);
+             System.out.println(action.length()-2);
+             String rowSelect = action.substring(0,action.length()-1);
              char columnSelect = action.charAt(action.length()-1);
-             //System.out.println(columnSelect + " " + action);
-
-             System.out.println(rowSelect + " " + columnSelect);
+             //String position = action.substring(2,action.length()-3);
              **/
+            lm.shootLaserAction(action);
+            System.out.println(nickname);
+            System.out.println(lm.toString());
         } else {
             System.out.println("the values entered exceed the allowed limit");
             shootLaserBeam();
+        }
+
+        subMenu();
+    }
+
+    //metodo para adivinar espejo
+
+    public void guessMirrorPosition() {
+        System.out.println("(1..n).Enter the row number\n" + "(A-Z).The column letter\n" +
+                "(R).Right or (L). Left in uppercase");
+        String select = scan.nextLine();
+        System.out.println(" ");
+        //agregar condicion de que se debe lanzar solo desde las filas y columnas principales
+        if (select.length() > 0) {
+            lm.selectNode(select);
+            System.out.println(nickname);
+            System.out.println(lm.toString());
+            /**
+             int rowSelect = Integer.parseInt(select.substring(0, select.length()-2));
+             char columnSelect = select.charAt(select.length()-2);
+             char position = select.charAt(select.length()-1);
+             System.out.println(rowSelect + " " + columnSelect + " " + position);
+             **/
+        } else {
+            System.out.println("the values entered exceed the allowed limit");
+            guessMirrorPosition();
         }
         subMenu();
     }
@@ -68,7 +92,7 @@ public class StartGame {
         System.out.println("3.Start menu");
 
         int option = Integer.parseInt(scan.nextLine());
-        if (option<4) {
+        if (option < 4) {
             switch (option) {
                 case 1:
                     shootLaserBeam();
@@ -80,34 +104,10 @@ public class StartGame {
                     new Menu();
                     break;
             }
-        }else {
+        } else {
             System.out.println("The option incorrect");
             subMenu();
         }
-    }
-
-    //metodo para adivinar espejo
-
-    public void guessMirrorPosition() {
-        System.out.println("(1..n).Enter the row number\n" + "(A-Z).The column letter\n" +
-                "(R).Right or (L). Left in uppercase");
-        String select = scan.nextLine();
-        System.out.println(" ");
-        //agregar condicion de que se debe lanzar solo desde las filas y columnas principales
-        if (select.length() > 0) {
-            lm.selectNode(select);
-            System.out.println(lm.toString());
-            /**
-            int rowSelect = Integer.parseInt(select.substring(0, select.length()-2));
-            char columnSelect = select.charAt(select.length()-2);
-            char position = select.charAt(select.length()-1);
-            System.out.println(rowSelect + " " + columnSelect + " " + position);
-            **/
-        } else {
-            System.out.println("the values entered exceed the allowed limit");
-            guessMirrorPosition();
-        }
-        subMenu();
     }
 }
 
